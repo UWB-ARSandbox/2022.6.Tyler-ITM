328cb050df3cb1dbc89c85961cc1493426fd7f4e
tchoi <tchoi94@uw.edu>
2023-04-23 23:31:22 -0700

    detailed output in release folder

--- HeightxAreaEstimate/Program.cs
+++ HeightxAreaEstimate/Program.cs
@@ -18,102 +18,164 @@ namespace HeightxAreaEstimate
 
         static async Task Main(string[] args)
         {
-            // Read the GeoJSON string from a text file
-            string filePath = "MarbleMt.txt";
-            List<object> objects = JsonFilter.ParseGeoJsonFile(filePath);
-            if (objects == null || objects.Count == 0)
+
+            // Create a StreamWriter for the output.log file
+            using (StreamWriter fileLogWriter = new StreamWriter("output.log", true))
             {
-                Console.WriteLine("Error reading GeoJSON data.");
-                return;
-            }
-
-            Console.WriteLine($"Total number of objects in the GeoJSON data: {objects.Count}");
-
-            // Extract the first LineString feature from the list of objects
-            LineString feature = objects.OfType<LineString>().FirstOrDefault();
-            if (feature == null)
-            {
-                Console.WriteLine("No LineString features found in the GeoJSON data.");
-                return;
-            }
-
-            Console.WriteLine($"Title of the LineString feature: {feature.Name}");
-            Console.WriteLine($"Total number of points in the LineString feature before filtering: {feature.Points.Count}");
-
-            Console.WriteLine("Processing GeoJSON coordinates...");
-
-            // Filter points by distance
-            List<Point> filteredPoints = new List<Point>();
-            double minDistance = 1; // Minimum distance in meters
-            filteredPoints.Add(feature.Points[0]);
-
-            for (int i = 1; i < feature.Points.Count; i++)
-            {
-                Point prevPoint = filteredPoints[filteredPoints.Count - 1];
-                Point currentPoint = feature.Points[i];
-                double distance = HaversineDistance(prevPoint.Latitude, prevPoint.Longitude, currentPoint.Latitude, currentPoint.Longitude);
-
-                if (distance >= minDistance)
+                // Create a StreamWriter for the console output
+                using (StreamWriter consoleLogWriter = new StreamWriter(Console.OpenStandardOutput()))
                 {
-                    filteredPoints.Add(currentPoint);
+                    consoleLogWriter.AutoFlush = true;
+                    Console.SetOut(consoleLogWriter);
+
+                    // Create a custom method to write to both console and file
+                    void Log(string message)
+                    {
+                        consoleLogWriter.WriteLine(message);
+                        fileLogWriter.WriteLine(message);
+                    }
+
+                    // Read the GeoJSON string from a text file
+                    string filePath = "MarbleMt.txt";
+                    List<object> objects = JsonFilter.ParseGeoJsonFile(filePath);
+                    if (objects == null || objects.Count == 0)
+                    {
+                        Log("Error reading GeoJSON data.");
+                        return;
+                    }
+
+                    Log($"Total number of objects in the GeoJSON data: {objects.Count}");
+
+                    // Extract all LineString features from the list of objects
+                    List<LineString> lineStringFeatures = objects.OfType<LineString>().ToList();
+                    if (lineStringFeatures.Count == 0)
+                    {
+                        Log("No LineString features found in the GeoJSON data.");
+                        return;
+                    }
+
+                    //// Extract the first LineString feature from the list of objects
+                    //LineString feature = objects.OfType<LineString>().FirstOrDefault();
+                    //if (feature == null)
+                    //{
+                    //    Log("No LineString features found in the GeoJSON data.");
+                    //    return;
+                    //}
+
+                    int featureIndex = 0;
+                    List<(string LineStringName, double AverageDifference, double MinDistance, int preFilterPoints, int postFilterPoints)> lineStringResults = new List<(string, double, double, int, int)>();
+
+                    foreach (LineString feature in lineStringFeatures)
+                    {
+                        featureIndex++;
+                        Log($"\nProcessing LineString feature {featureIndex} of {lineStringFeatures.Count}");
+
+
+                        Log($"\tTitle of the LineString feature: {feature.Name}");
+                        Log($"\tTotal number of points in the LineString feature before filtering: {feature.Points.Count}");
+
+                        Log("Processing GeoJSON coordinates...");
+
+                        // Filter points by distance
+                        List<Point> filteredPoints = new List<Point>();
+                        double minDistance = 2; // Minimum distance in meters
+                        filteredPoints.Add(feature.Points[0]);
+
+                        for (int i = 1; i < feature.Points.Count; i++)
+                        {
+                            Point prevPoint = filteredPoints[filteredPoints.Count - 1];
+                            Point currentPoint = feature.Points[i];
+                            double distance = HaversineDistance(prevPoint.Latitude, prevPoint.Longitude, currentPoint.Latitude, currentPoint.Longitude);
+
+                            if (distance >= minDistance)
+                            {
+                                filteredPoints.Add(currentPoint);
+                            }
+                        }
+
+                        int preFilteredPoints = feature.Points.Count;
+                        feature.Points = filteredPoints;
+                        Log($"\tTotal number of points in the LineString feature after filtering: {feature.Points.Count}");
+
+                        int batchSize = 100;
+                        int batchCount = (int)Math.Ceiling((double)feature.Points.Count / batchSize);
+
+                        Log($"Minimum distance: {minDistance} meters");
+                        Log($"Batch size: {batchSize}");
+                        Log($"Title of the LineString feature: {feature.Name}");
+
+                        // Get elevation data
+                        LineString updatedFeature = await GetElevationDataFromOpenTopoData(feature, minDistance, 100);
+                        feature.Points = updatedFeature.Points;
+
+                        double totalDifference = 0;
+                        int totalProcessedPoints = 0;
+
+
+                        for (int batchIndex = 0; batchIndex < batchCount; batchIndex++)
+                        {
+                            int startIndex = batchIndex * batchSize;
+                            int endIndex = Math.Min(startIndex + batchSize, feature.Points.Count);
+                            int currentBatchSize = endIndex - startIndex;
+
+                            double total2DDistance = 0;
+                            double total3DDistance = 0;
+
+                            for (int i = startIndex; i < endIndex - 1; i++)
+                            {
+                                Point point1 = feature.Points[i];
+                                Point point2 = feature.Points[i + 1];
+
+                                double twoDimensionalDistance = Calculate2DDistance(point1.Latitude, point1.Longitude, point2.Latitude, point2.Longitude);
+                                double threeDimensionalDistance = Calculate3DDistance(point1.Latitude, point1.Longitude, point1.Altitude, point2.Latitude, point2.Longitude, point2.Altitude);
+
+                                total2DDistance += twoDimensionalDistance;
+                                total3DDistance += threeDimensionalDistance;
+
+                                double difference = Math.Abs(twoDimensionalDistance - threeDimensionalDistance);
+                                totalDifference += difference;
+                            }
+
+                            Log($"Batch {batchIndex + 1}:");
+                            Log($"  2D Distance: {total2DDistance} meters");
+                            Log($"  3D Distance: {total3DDistance} meters");
+                            Log($"  Difference between 2D and 3D distances: {Math.Abs(total2DDistance - total3DDistance)} meters");
+                        }
+
+                        totalProcessedPoints = feature.Points.Count - 1; // Subtract 1 because the difference is calculated between pairs of points
+                        double averageDifference = totalDifference / totalProcessedPoints;
+
+                        Log($"\n\nAverage difference between 2D and 3D distances for LineString feature {featureIndex}: {feature.Name} --> {averageDifference} meters\n");
+
+                        lineStringResults.Add((feature.Name, averageDifference, minDistance, preFilteredPoints, filteredPoints.Count));
+
+                        // Save the updated GeoJSON with elevation data
+                        string updatedGeoJsonString = JsonConvert.SerializeObject(feature, Formatting.Indented);
+                        File.WriteAllText("output_with_elevation.geojson", updatedGeoJsonString);
+
+                        // Save the original GeoJSON without elevation data
+                        string originalGeoJsonString = JsonConvert.SerializeObject(feature, Formatting.Indented, new JsonSerializerSettings
+                        {
+                            ContractResolver = new IgnorePropertyResolver("Altitude")
+                        });
+                        File.WriteAllText("output_without_elevation.geojson", originalGeoJsonString);
+
+                    }
+                    // Write the LineString results to a text file
+                    using (StreamWriter outputFile = new StreamWriter("LineStringResults.txt"))
+                    {
+                        outputFile.WriteLine("LineString Name, Average Difference (2D vs 3D), Minimum Distance, Pre-filter Points, Post-filter Points");
+                        foreach (var result in lineStringResults)
+                        {
+                            outputFile.WriteLine($"{result.LineStringName}, {result.AverageDifference}, {result.MinDistance}, {result.preFilterPoints}, {result.postFilterPoints}");
+                        }
+                    }
+
+
+                    Log("Program complete. Press Enter to End.");
+                    Console.ReadLine();
                 }
             }
-
-            feature.Points = filteredPoints;
-            Console.WriteLine($"Total number of points in the LineString feature after filtering: {feature.Points.Count}");
-
-            int batchSize = 100;
-            int batchCount = (int)Math.Ceiling((double)feature.Points.Count / batchSize);
-
-            Console.WriteLine($"Minimum distance: {minDistance} meters");
-            Console.WriteLine($"Batch size: {batchSize}");
-            Console.WriteLine($"Title of the LineString feature: {feature.Name}");
-
-            
-            // Get elevation data
-            feature = await GetElevationDataFromOpenTopoData(feature, minDistance, 100);
-
-            for (int batchIndex = 0; batchIndex < batchCount; batchIndex++)
-            {
-                int startIndex = batchIndex * batchSize;
-                int endIndex = Math.Min(startIndex + batchSize, feature.Points.Count);
-                int currentBatchSize = endIndex - startIndex;
-
-                double total2DDistance = 0;
-                double total3DDistance = 0;
-
-                for (int i = startIndex; i < endIndex - 1; i++)
-                {
-                    Point point1 = feature.Points[i];
-                    Point point2 = feature.Points[i + 1];
-
-                    double twoDimensionalDistance = Calculate2DDistance(point1.Latitude, point1.Longitude, point2.Latitude, point2.Longitude);
-                    double threeDimensionalDistance = Calculate3DDistance(point1.Latitude, point1.Longitude, point1.Altitude, point2.Latitude, point2.Longitude, point2.Altitude);
-
-                    total2DDistance += twoDimensionalDistance;
-                    total3DDistance += threeDimensionalDistance;
-                }
-
-                Console.WriteLine($"Batch {batchIndex + 1}:");
-                Console.WriteLine($"  2D Distance: {total2DDistance} meters");
-                Console.WriteLine($"  3D Distance: {total3DDistance} meters");
-                Console.WriteLine($"  Difference between 2D and 3D distances: {Math.Abs(total2DDistance - total3DDistance)} meters");
-            }
-
-
-            // Save the updated GeoJSON with elevation data
-            string updatedGeoJsonString = JsonConvert.SerializeObject(feature, Formatting.Indented);
-            File.WriteAllText("output_with_elevation.geojson", updatedGeoJsonString);
-
-            // Save the original GeoJSON without elevation data
-            string originalGeoJsonString = JsonConvert.SerializeObject(feature, Formatting.Indented, new JsonSerializerSettings
-            {
-                ContractResolver = new IgnorePropertyResolver("Altitude")
-            });
-            File.WriteAllText("output_without_elevation.geojson", originalGeoJsonString);
-
-            Console.WriteLine("Program complete. Press Enter to End.");
-            Console.ReadLine();
         }
 
         public static async Task<LineString> GetElevationDataFromOpenTopoData(LineString feature, double minDistance, int batchSize = 100)
